a77d5a08d0c465c59e33b3aaecb368ea30497843
Nitesh Kumar <niteshkr9155166021@gmail.com>
2024-10-15 16:34:55 +0530

    Added page 4 and 5

--- script.js
+++ script.js
@@ -203,3 +203,178 @@ ScrollTrigger.create({
 });
 }
 canvas()
+
+
+
+var clutter = "";
+
+document.querySelector("#page4>h1").textContent.split("").forEach(function(dets){
+    clutter += `<span>${dets}</span>`
+
+    document.querySelector("#page4>h1").innerHTML = clutter;
+})
+
+gsap.to("#page4>h1>span",{
+    scrollTrigger:{
+        trigger:`#page4>h1>span`,
+        start:`top bottom`,
+        end:`bottom top`,
+        scroller:`#main`,
+        scrub:.5,
+    },
+    stagger:.2,
+    color:`#fff`
+})
+
+
+
+function canvas1(){
+    const canvas = document.querySelector("#page5>canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  render();
+});
+
+function files(index) {
+  var data = `
+  ./bridges00004.png
+  ./bridges00007.png
+  ./bridges00010.png
+  ./bridges00013.png
+  ./bridges00016.png
+  ./bridges00019.png
+  ./bridges00022.png
+  ./bridges00025.png
+  ./bridges00028.png
+  ./bridges00031.png
+  ./bridges00034.png
+  ./bridges00037.png
+  ./bridges00040.png
+  ./bridges00043.png
+  ./bridges00046.png
+  ./bridges00049.png
+  ./bridges00052.png
+  ./bridges00055.png
+  ./bridges00058.png
+  ./bridges00061.png
+  ./bridges00064.png
+  ./bridges00067.png
+  ./bridges00070.png
+  ./bridges00073.png
+  ./bridges00076.png
+  ./bridges00079.png
+  ./bridges00082.png
+  ./bridges00085.png
+  ./bridges00088.png
+  ./bridges00091.png
+  ./bridges00094.png
+  ./bridges00097.png
+  ./bridges00100.png
+  ./bridges00103.png
+  ./bridges00106.png
+  ./bridges00109.png
+  ./bridges00112.png
+  ./bridges00115.png
+  ./bridges00118.png
+  ./bridges00121.png
+  ./bridges00124.png
+  ./bridges00127.png
+  ./bridges00130.png
+  ./bridges00133.png
+  ./bridges00136.png
+  ./bridges00139.png
+  ./bridges00142.png
+  ./bridges00145.png
+  ./bridges00148.png
+  ./bridges00151.png
+  ./bridges00154.png
+  ./bridges00157.png
+  ./bridges00160.png
+  ./bridges00163.png
+  ./bridges00166.png
+  ./bridges00169.png
+  ./bridges00172.png
+  ./bridges00175.png
+  ./bridges00178.png
+  ./bridges00181.png
+  ./bridges00184.png
+  ./bridges00187.png
+  ./bridges00190.png
+  ./bridges00193.png
+  ./bridges00196.png
+  ./bridges00199.png
+  ./bridges00202.png
+ `;
+  return data.split("\n")[index];
+}
+
+const frameCount = 67;
+
+const images = [];
+const imageSeq = {
+  frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = files(i);
+  images.push(img);
+}
+
+gsap.to(imageSeq, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: `none`,
+  scrollTrigger: {
+    scrub: .5,
+    trigger: `#page5`,
+    start: `top top`,
+    end: `250% top`,
+    scroller: `#main`,
+  },
+  onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+  scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.max(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
+ScrollTrigger.create({
+
+  trigger: "#page5",
+  pin: true,
+  scroller: `#main`,
+  start: `top top`,
+  end: `250% top`,
+});
+}
+canvas1()
